c07ed515a9bc3d574562778856050c5bad879e7a
Karl Rohr <karlrohr@annihilation.local>
2016-06-01 19:29:50 -0400

    added chai for mocha functionality

---
+++ project/test/__api.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+describe('Cows in my field', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
