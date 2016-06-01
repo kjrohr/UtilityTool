3585ab1c735dee7e4100b284f78854767ec29327
Karl Rohr <karlrohr@annihilation.local>
2016-06-01 19:35:55 -0400

    added expect dev dependency and changed the test __api.js file

--- project/test/__api.js
+++ project/test/__api.js
@@ -1,9 +1,6 @@
-var assert = require('chai').assert;
-describe('Cows in my field', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+var expect = require('expect');
+describe('API', function(){
+  it('Should probably load, somehow', function(){
+    expect('daisy').toBe('daisy');
   });
 });
