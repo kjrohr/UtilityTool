fae8d75e275b1f8389d86eb2fe2d68fbff6c5559
Karl Rohr <karlrohr@annihilation.local>
2016-06-01 19:06:52 -0400

    Added comments to src/server.js

--- project/src/server.js
+++ project/src/server.js
@@ -1,12 +1,19 @@
+// Sets variable express to require the express module
 var express = require('express');
+
+// Sets variable body_parser to require the body-parser module
+//var body_parser = require('body-parser');
+
+// Sets variable app to encompass express
 var app = express();
 
+// Config
 var port = 3000;
-
-app.get('/', function(req,res){
-  res.json({health: true})
-});
-
 app.listen(port, function(){
   console.log("Our server is running on port: " + port + ".");
+});
+
+// Route
+app.get('/', function(req,res){
+  res.json({health: true})
 });
