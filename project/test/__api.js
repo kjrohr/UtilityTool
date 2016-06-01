4749e42869e7ca257788530b680478c17d10d271
Karl Rohr <karlrohr@annihilation.local>
2016-06-01 19:40:32 -0400

    Added more unit testing things, modifiying server with beforeEach and afterEach

--- project/test/__api.js
+++ project/test/__api.js
@@ -1,6 +1,18 @@
 var expect = require('expect');
 var server = require('../src/server.js');
+// Overwrites request with supertest module
+var request = require('supertest');
 describe('API', function(){
+  var server;
+  beforeEach(function(){
+    server = require('../src/server.js');
+  });
+
+  afterEach(function(){
+    server.close();    
+  });
+
+
   it('Should probably load, somehow', function(){
     expect('daisy').toBe('daisy');
   });
